bf718c02a0d61ee421306ae8edd1177943e5c7b8
GabCardoso <gabrielacfontes.gc@gmail.com>
2020-02-21 11:41:03 -0300

    Add files via upload

---
+++ node/globalCliente.js
@@ -0,0 +1,6 @@
+require('./global')
+
+console.log(MinhaApp.saudacao())
+
+MinhaApp.nome = 'Mudou'
+console.log(MinhaApp.nome)
---
+++ node/moduloA.js
@@ -0,0 +1,3 @@
+this.ola = 'Eai'
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Até'
